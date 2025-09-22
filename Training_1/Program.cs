4838bf2ba3c1dfab6e7b617e98eb8d7071b1c17b
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-22 15:43:50 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,7 +8,30 @@
 namespace Training_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         int number1;
+         int number2;
+
+         Console.WriteLine ("Type in a number");
+         int.TryParse (Console.ReadLine (), out number1);
+         Console.WriteLine ("Type in the second number");
+         int.TryParse (Console.ReadLine (), out number2);
+
+         int num1 = number1;
+         int num2 = number2;
+         int remainder;
+
+         do {
+
+            remainder = number1 % number2;
+            number1 = number2;
+            number2 = remainder;
+
+
+
+         } while (remainder != 0);
+
+         Console.WriteLine ($"\nGCD: {number1}");
+         Console.WriteLine ($"LCM: {(num1 * num2) / number1}");
       }
    }
 }

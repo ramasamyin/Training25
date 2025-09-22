7b6d1dcb9e8f5ecd731a1a05510acf6eb443d715
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-22 17:13:22 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,7 +8,29 @@
 namespace Training_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         int number = 0;
+         Console.WriteLine ("Give an input number");
+         bool validity = int.TryParse (Console.ReadLine (), out number);
+         if (!validity) {
+            Console.WriteLine ("You have not entered a valid number");
+         } else if (number < 0) {
+            Console.WriteLine ("Enter a non-negative number");
+         } else {
+            if (Armstrong (number)) { Console.WriteLine ($"Yes. {number} is an Armstrong Number "); } else { Console.WriteLine ($"No. {number} is not an Armstrong number"); }
+         }
+
+      }
+      static bool Armstrong (int n1) {
+         int sum = 0;
+         int length = n1.ToString ().Length;
+         foreach (char c in n1.ToString ()) {
+            int digit = c - '0';
+            sum += (int)Math.Pow (digit, length);
+
+         }
+
+         return n1 == sum;
+
       }
    }
 }

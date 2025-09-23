6d334e584a2074279e1be912cc7a319c6aa42da7
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-23 11:06:18 +0530

    Initial Commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,7 +8,36 @@
 namespace Training_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         int number = 0;
+         Console.WriteLine ("Enter n (Which Armstrong number do you want) : ");
+         int.TryParse (Console.ReadLine (), out number);
+         FindNthArmstrong (number);
+
+      }
+
+      static void FindNthArmstrong (int nth) {
+
+         int count = 0;
+
+         for (int n1 = 0; ; n1++) {
+            int sum = 0;
+            int length = n1.ToString ().Length;
+            foreach (char c in n1.ToString ()) {
+               int digit = c - '0';
+               sum += (int)Math.Pow (digit, length);
+
+            }
+            if (n1 == sum) {
+               count++;
+               if (count == nth) {
+                  Console.WriteLine ($"\nThe Armstrong number - {nth} is {n1}");
+                  return;
+               }
+            }
+
+         }
+
+
       }
    }
 }

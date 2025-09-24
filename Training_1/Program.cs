45d21a4609ca626bf9587132e150d6df2045a180
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-24 13:03:59 +0530

    Review fix 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,21 +8,15 @@
 namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
-      int number = 0;
       Console.WriteLine ("Give an input number");
-      if (!int.TryParse (Console.ReadLine (), out number)) Console.WriteLine ("You have not entered a valid number");
+      if (!int.TryParse (Console.ReadLine (), out int number)) Console.WriteLine ("You have not entered a valid number");
       else if (number < 0) Console.WriteLine ("Enter a non-negative number");
-      else {
-         if (Armstrong (number)) Console.WriteLine ($"\nArmstrong Number ");
-         else Console.WriteLine ($"\nNot an Armstrong number");
-      }
+      else Console.WriteLine (Armstrong (number) ? "Armstrong Number" : "Not an Armstrong Number");
    }
    static bool Armstrong (int n) {
-      int sum = 0;
-      int length = n.ToString ().Length;
+      (int sum, int length) = (0, n.ToString ().Length);
       foreach (char c in n.ToString ()) {
-         int digit = c - '0';
-         sum += (int)Math.Pow (digit, length);
+         sum += (int)Math.Pow (c - '0', length);
       }
       return n == sum;
    }

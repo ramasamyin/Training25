95bebd8d002777a69646e091433cd8a607a30653
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-24 12:00:14 +0530

    Review Fix

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,28 +5,26 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training_1 {
-   internal class Program {
-      static void Main (string[] args) {
-         int number = 0;
-         Console.WriteLine ("Give an input number");
-         bool validity = int.TryParse (Console.ReadLine (), out number);
-         if (!validity) {
-            Console.WriteLine ("You have not entered a valid number");
-         } else if (number < 0) {
-            Console.WriteLine ("Enter a non-negative number");
-         } else {
-            if (Armstrong (number)) { Console.WriteLine ($"Yes. {number} is an Armstrong Number "); } else { Console.WriteLine ($"No. {number} is not an Armstrong number"); }
-         }
-      }
-      static bool Armstrong (int n1) {
-         int sum = 0;
-         int length = n1.ToString ().Length;
-         foreach (char c in n1.ToString ()) {
-            int digit = c - '0';
-            sum += (int)Math.Pow (digit, length);
-         }
-         return n1 == sum;
+namespace Training_1;
+internal class Program {
+   static void Main (string[] args) {
+      int number = 0;
+      Console.WriteLine ("Give an input number");
+      if (!int.TryParse (Console.ReadLine (), out number)) Console.WriteLine ("You have not entered a valid number");
+      else if (number < 0) Console.WriteLine ("Enter a non-negative number");
+      else {
+         if (Armstrong (number)) Console.WriteLine ($"\nArmstrong Number ");
+         else Console.WriteLine ($"\nNot an Armstrong number");
       }
    }
+   static bool Armstrong (int n) {
+      int sum = 0;
+      int length = n.ToString ().Length;
+      foreach (char c in n.ToString ()) {
+         int digit = c - '0';
+         sum += (int)Math.Pow (digit, length);
+      }
+      return n == sum;
+   }
 }
+

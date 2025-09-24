31bbb5f3a55bee1d268eebe6d8b2ecce2c8577a7
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-24 15:35:53 +0530

    Second commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,13 +5,13 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training_1 {
-   internal class Program {
-      static void Main (string[] args) {
-         for (int i = 1; i <= 10; i++) {
-            for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i,2} * {j,2} = {i * j,3}");
-            Console.WriteLine ();
-         }
+namespace Training_1;
+internal class Program {
+   static void Main (string[] args) {
+      for (int i = 1; i <= 10; i++) {
+         for (int j = 1; j <= 10; j++) Console.WriteLine ($"{i,2} * {j,2} = {i * j,3}");
+         Console.WriteLine ();
       }
    }
 }
+

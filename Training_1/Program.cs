b10c2dea61febbe3c1341fa4a99354c81ef8793f
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-24 17:15:19 +0530

    Second Commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -11,14 +11,16 @@ namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter a string which has to be reversed: ");
-      string word = Console.ReadLine ();
-      char[] chars = word.ToCharArray ();
-      Array.Reverse (chars);
-      for (int i = 0; i < word.Length; i++) {
-         if (char.IsUpper (word[i])) chars[i] = char.ToUpper (chars[i]);
-         else if (char.IsLower (word[i])) chars[i] = char.ToLower (chars[i]);
-      }
-      Console.WriteLine (chars);
+      string? word = Console.ReadLine ();
+      if (!string.IsNullOrEmpty (word)) {
+         char[] chars = word.ToCharArray ();
+         Array.Reverse (chars);
+         for (int i = 0; i < word.Length; i++) {
+            if (char.IsUpper (word[i])) chars[i] = char.ToUpper (chars[i]);
+            else if (char.IsLower (word[i])) chars[i] = char.ToLower (chars[i]);
+         }
+         Console.WriteLine (chars);
+      } else Console.WriteLine ("No input provided.");
    }
 }
 

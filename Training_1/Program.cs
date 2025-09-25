a840897feda41e159eb34e4da4305a0edfa0484c
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-25 15:32:31 +0530

    Review Fix 5

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -14,15 +14,15 @@ internal class Program {
       Print ("Enter the second number: ");
       while (!int.TryParse (Console.ReadLine (), out n2)) Print ("The input is not valid.\nEnter another number: ");
       if (n1 == 0 || n2 == 0) {
-         Print ("\nGCD: 0");
-         Print ("LCM: 0");
+         Print ("\nGCD: 0\nLCM: 0");
       } else {
          var (num1, num2) = (n1, n2);
-         int rem;
-         do {
+         int rem = -1;
+         while (rem != 0) {
             rem = n1 % n2;
             (n1, n2) = (n2, rem);
-         } while (rem != 0);
+         }
+         ;
          Print ($"\nGCD: {n1}\nLCM: {(num1 * num2) / n1}");
       }
    }

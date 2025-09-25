ddcf581045e76cae2497106ecbb52ded1ec796f2
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-25 11:59:30 +0530

    Review Fix 7

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,25 +5,25 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
+using System;
 using System.Text;
 namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Give a number as input");
+      Console.Write ("Enter a number: ");
       int n;
-      StringBuilder sb = new StringBuilder ();
-      while (!int.TryParse (Console.ReadLine (), out n)) Console.WriteLine ("\nThe number is not valid.");
+      StringBuilder sb = new ();
+      while (!int.TryParse (Console.ReadLine (), out n)) Console.Write ("The number is not valid.\nEnter a number :");
       // Binary Conversion
-      List<int> binaries = [];
       int tmp = n;
       while (tmp > 0) {
          sb.Insert (0, tmp % 2);
          tmp /= 2;
       }
       // Hexadecimal Conversion
-      List<string> hexaDecimals = [];
       tmp = n;
-      StringBuilder sbhexa = new StringBuilder ();
+      Console.WriteLine ($"\nInput: {n}\nBinary: {sb}");
+      sb.Clear ();
       while (tmp > 0) {
          string str = (tmp % 16) switch {
             10 => "A",
@@ -34,10 +34,10 @@ internal class Program {
             15 => "F",
             _ => (tmp % 16).ToString ()
          };
-         sbhexa.Insert (0, str);
+         sb.Insert (0, str);
          tmp /= 16;
       }
-      Console.WriteLine ($"\nInput: {n}\nBinary: {sb}\nHexadecimal: {sbhexa}");
+      Console.WriteLine ($"Hexadecimal: {sb}");
    }
 }
 

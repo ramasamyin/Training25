0a46513125fc5ef13d78ab6369551fbfcd76f2bd
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-25 14:38:33 +0530

    Review Fix 4

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,27 +5,29 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training_1 {
-   internal class Program {
-      static void Main (string[] args) {
-         int n1; int n2;
-         Console.Write ("Enter the first number: ");
-         while (!int.TryParse (Console.ReadLine (), out n1)) Console.Write ("The input is not valid.\nEnter another number: ");
-         Console.Write ("Enter the second number: ");
-         while (!int.TryParse (Console.ReadLine (), out n2)) Console.Write ("The input is not valid.\nEnter another number: ");
-         if (n1 == 0 || n2 == 0) {
-            Console.WriteLine ("GCD: 0");
-            Console.WriteLine ("LCM: 0");
-         } else {
-            var (num1, num2) = (n1, n2);
-            int rem;
-            do {
-               rem = n1 % n2;
-               (n1, n2) = (n2, rem);
-            } while (rem != 0);
-            Console.WriteLine ($"\nGCD: {n1}\nLCM: {(num1 * num2) / n1}");
-         }
+namespace Training_1;
+internal class Program {
+   static void Main (string[] args) {
+      int n1, n2;
+      Print ("Enter the first number: ");
+      while (!int.TryParse (Console.ReadLine (), out n1)) Print ("The input is not valid.\nEnter another number: ");
+      Print ("Enter the second number: ");
+      while (!int.TryParse (Console.ReadLine (), out n2)) Print ("The input is not valid.\nEnter another number: ");
+      if (n1 == 0 || n2 == 0) {
+         Print ("\nGCD: 0");
+         Print ("LCM: 0");
+      } else {
+         var (num1, num2) = (n1, n2);
+         int rem;
+         do {
+            rem = n1 % n2;
+            (n1, n2) = (n2, rem);
+         } while (rem != 0);
+         Print ($"\nGCD: {n1}\nLCM: {(num1 * num2) / n1}");
       }
    }
+
+   static void Print (string str) => Console.Write (str);
 }
+
 

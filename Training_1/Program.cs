e095f3ae3635b9773410e35bcf2cacba02f6c0a5
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-25 15:36:50 +0530

    Review Fix 9

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -15,9 +15,9 @@ internal class Program {
    }
 
    static bool Armstrong (int n) {
-      string num = n.ToString ();
-      var (sum, len) = (0, num.Length);
-      foreach (char c in num) sum += (int)Math.Pow (c - '0', len);
+      string str = n.ToString ();
+      var (sum, len) = (0, str.Length);
+      foreach (char c in str) sum += (int)Math.Pow (c - '0', len);
       return n == sum;
    }
 

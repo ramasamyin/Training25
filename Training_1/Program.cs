32aa79732b796218219aa67bf6ea34d2c331e565
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-26 16:05:07 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,7 +8,21 @@
 namespace Training_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Console.Write ("Enter an input: ");
+         string? input = Console.ReadLine ();
+         if (String.IsNullOrEmpty (input)) Console.WriteLine ("No input is entered");
+         else {
+            FindWinner (input.ToUpper (), out char winner, out int wVotes);
+            Console.WriteLine ($"{winner},{wVotes}");
+         }
+
+      }
+
+      static void FindWinner (string votes, out char winner, out int wVotes) {
+         List<int> myInts = [];
+         foreach (char c in votes) myInts.Add ((votes.Count (a => a == c)));
+         wVotes = myInts.Max ();
+         winner = votes[wVotes];
       }
    }
 }

2ffae1a06cf19f0ea466c50a877063ea903663f0
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-26 14:34:05 +0530

    Review Fix 4

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,28 +5,31 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
+using System.Diagnostics.CodeAnalysis;
+
 namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
       Console.WriteLine ("Enter n - Which Armstrong number do you want (upto 25) : ");
       int.TryParse (Console.ReadLine (), out int n);
-      FindNthArmstrong (n);
+      Console.WriteLine ($"\nThe {n}th Armstrong Number is {FindNthArmstrong (n)}. ");
    }
 
-   //0 is included as the first Armstrong Number
-   static void FindNthArmstrong (int n) {
+   //0 is the first Armstrong Number
+   static int FindNthArmstrong (int n) {
       int count = 0;
       for (int i = 0; ; i++) {
-         var (sum, len) = (0, i.ToString ().Length);
-         for (int j = i; j > 0; j /= 10) sum += (int)Math.Pow (j % 10, len);
-         if (i == sum) {
+         if (IsArmstrong (i)) {
             count++;
-            if (count == n) {
-               Console.WriteLine ($"\nThe {n}th Armstrong number is {i}");
-               return;
-            }
+            if (count == n) return i;
          }
       }
+   }
+
+   static bool IsArmstrong (int n) {
+      int sum = 0;
+      for (int i = n; i > 0; i /= 10) sum += (int)Math.Pow (i % 10, n.ToString ().Length);
+      return n == sum;
    }
 }
 

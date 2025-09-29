f34f08278ce4d4285562f1c88a720de1f7bcd2d7
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-09-29 15:46:35 +0530

    Review Fix 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -3,22 +3,21 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T06 branch.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Enter a number (must be 2 digits) for which the digital root has to be found");
+      Write ("Enter a number: ");
       int n;
-      while (!int.TryParse (Console.ReadLine (), out n)) Console.WriteLine ("Please enter a valid number");
+      while (!int.TryParse (ReadLine (), out n)) WriteLine ("Please enter a valid number");
       while (n >= 10) {
          int sum = 0;
-         foreach (char c in n.ToString ()) {
-            sum += c - '0';
-         }
+         for (int i = n; i>0 ; i/=10) sum += (i%10);
          n = sum;
       }
-      Console.WriteLine ($"\nThe digital root is {n}");
+      WriteLine ($"\nThe digital root is {n}");
    }
 }
 

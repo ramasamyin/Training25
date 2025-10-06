6aca1f49ffb632b3910faaacd07caed3ed01f0a3
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-06 14:13:41 +0530

    Review Fix 3

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -3,15 +3,16 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T05 branch.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training_1;
 
 internal class Program {
    static void Main (string[] args) {
-      for (int i = 1; i <= 10; i++) {
-         for (int j = 1; j <= 10; j++) WriteLine ($"{i,2} * {j,2} = {i * j}");
+      int n = 10;
+      for (int i = 1; i <= n; i++) {
+         for (int j = 1; j <= n; j++) WriteLine ($"{i,2} * {j,2} = {i * j}");
          WriteLine ();
       }
    }

df5873492ffd73ccbc40430ff320b4ed13b0ec35
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-06 12:28:33 +0530

    Review Fix 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -6,23 +6,26 @@
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
 namespace Training_1;
+using static System.Console;
+
 internal class Program {
    static void Main (string[] args) {
-      Console.Write ("Enter the number of rows upto which you wish to print the Pascal's triangle: ");
-      int.TryParse (Console.ReadLine (), out int rows);
-      Console.WriteLine ($"\nPascal's Triangle for {rows} rows: ");
-      for (int x = 0; x < rows; x++) {
-         for (int s = 0; s < rows - x - 1; s++) Console.Write (" ");
-         for (int y = 0; y <= x; y++) Console.Write ($"{Combination (x, y)} ");
-         Console.WriteLine ();
+      Write ("Enter the number of rows upto which you wish to print the Pascal's triangle: ");
+      int.TryParse (ReadLine (), out int r);
+      WriteLine ($"\nPascal's Triangle for {r} rows ");
+      for (int i = 0; i < r; i++) {
+         for (int j = 0; j < r - i - 1; j++) Write (" ");
+         for (int k = 0; k <= i; k++) Write ($"{Combination (i, k)} ");
+         WriteLine ("");
       }
    }
 
+   //Returns the combination value C(n, r)
    static long Combination (int n, int r) {
       if (r > n) return 0;
       if (r == 0 || r == n) return 1;
       long result = 1;
-      for (int i = 1; i <= r; i++) result = result * (n - i + 1) / i;
+      for (int i = 1; i <= r; i++) result *= (n - i + 1) / i;
       return result;
    }
 }

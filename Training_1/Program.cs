8da992d465ad759b8c5b2eb46c29e09671eb3ead
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-06 11:54:03 +0530

    Review Fix

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,16 +8,16 @@
 namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
-      Console.WriteLine ("Enter the number of rows upto which you wish to print the Pascal's triangle");
+      Console.Write ("Enter the number of rows upto which you wish to print the Pascal's triangle: ");
       int.TryParse (Console.ReadLine (), out int rows);
-      Console.Write ($"\nPascal's Triangle for {rows} rows can be given as: ");
-      Console.WriteLine ();
+      Console.WriteLine ($"\nPascal's Triangle for {rows} rows: ");
       for (int x = 0; x < rows; x++) {
          for (int s = 0; s < rows - x - 1; s++) Console.Write (" ");
          for (int y = 0; y <= x; y++) Console.Write ($"{Combination (x, y)} ");
          Console.WriteLine ();
       }
    }
+
    static long Combination (int n, int r) {
       if (r > n) return 0;
       if (r == 0 || r == n) return 1;

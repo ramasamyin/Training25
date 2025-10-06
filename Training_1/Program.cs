c6cb41d6a7c155a79f8ab3df0dffd72ff3388a29
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-06 16:45:07 +0530

    Review Fix 4

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -27,7 +27,7 @@ internal class Program {
       if (r > n) return 0;
       if (r == 0 || r == n) return 1;
       int result = 1;
-      for (int i = 1; i <= r; i++) result *= (n - i + 1) / i;
+      for (int i = 1; i <= r; i++) result = result * (n - i + 1) / i;
       return result;
    }
 }

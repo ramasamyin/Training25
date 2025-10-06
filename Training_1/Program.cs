60a90b214586f9918e7d9561e28625a422a29b31
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-06 13:08:03 +0530

    Review Fix 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -11,7 +11,7 @@ namespace Training_1;
 internal class Program {
    static void Main (string[] args) {
       for (int i = 1; i <= 10; i++) {
-         for (int j = 1; j <= 10; j++) WriteLine ($"{i,2} * {j,2} = {i * j,3}");
+         for (int j = 1; j <= 10; j++) WriteLine ($"{i,2} * {j,2} = {i * j}");
          WriteLine ();
       }
    }

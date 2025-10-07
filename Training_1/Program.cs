2847f0e587c79567df106382b960abde2f6f61ad
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-07 15:03:20 +0530

    Review Fix 7

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -19,10 +19,8 @@ internal class Program {
       }
    }
 
-   //Each element in the pascal's triangle is calculated
-   //using the combination formula (nCr), where:
-   //- n is the row number,
-   //- r is the position of the element within the row.
+   //Each element in the pascal's triangle is calculated using the combination formula (nCr)
+   //where,n is the row number and r is the position of the element within the row
    static int Combination (int n, int r) {
       if (r > n) return 0;
       if (r == 0 || r == n) return 1;

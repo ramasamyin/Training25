7f48ee3f3b5385c5755ba8ae36b51bcf2c552b8a
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-07 11:32:26 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,10 +5,24 @@
 // Program.cs
 // Program on main branch.
 // ------------------------------------------------------------------------------------------------
+using static System.Console;
 namespace Training_1 {
    internal class Program {
       static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+         Write ("Enter a number: ");
+         int.TryParse (ReadLine (), out int n);
+         int minSum = int.MaxValue;
+         for (int k = 0; k < 10; k++) {
+            int temp = n;
+            int sum = 0;
+            while (temp > 0) {
+               int digit = temp % 10;
+               sum += Math.Abs (digit - k);
+               temp /= 10;
+            }
+            if (sum < minSum) minSum = sum;
+         }
+         WriteLine (minSum);
       }
    }
 }

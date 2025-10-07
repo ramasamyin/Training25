02e6119d1d8cf90bbac2fd930a235bdd42f12ec1
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-07 11:56:09 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -3,26 +3,29 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T14 branch.
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
-namespace Training_1 {
-   internal class Program {
-      static void Main (string[] args) {
-         Write ("Enter a number: ");
-         int.TryParse (ReadLine (), out int n);
-         int minSum = int.MaxValue;
-         for (int k = 0; k < 10; k++) {
-            int temp = n;
-            int sum = 0;
-            while (temp > 0) {
-               int digit = temp % 10;
-               sum += Math.Abs (digit - k);
-               temp /= 10;
-            }
-            if (sum < minSum) minSum = sum;
+namespace Training_1;
+internal class Program {
+   static void Main (string[] args) {
+      Write ("Enter a number: ");
+      int.TryParse (ReadLine (), out int n);
+      WriteLine (MinTransform (n));
+   }
+
+   static int MinTransform (int num) {
+      int minSum = int.MaxValue;
+      for (int k = 0; k < 10; k++) {
+         int temp = num;
+         int sum = 0;
+         while (temp > 0) {
+            int digit = temp % 10;
+            sum += Math.Abs (digit - k);
+            temp /= 10;
          }
-         WriteLine (minSum);
+         if (sum < minSum) minSum = sum;
       }
+      return minSum;
    }
 }

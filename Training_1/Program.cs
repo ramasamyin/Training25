9ab9260e2267d66ef8da2aa29c83eb9b4189cb97
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-08 15:11:17 +0530

    Review Fix

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -5,31 +5,29 @@
 // Program.cs
 // Program on T10 branch.
 // ------------------------------------------------------------------------------------------------
-using System.Text;
 using static System.Console;
 namespace Training_1;
 
 internal class Program {
    static void Main (string[] args) {
-      WriteLine ("Enter a string which has to be reversed: ");
+      Write ("Enter a string which has to be reversed: ");
       string? word = ReadLine ();
       if (!string.IsNullOrEmpty (word)) {
          char[] chars = word.ToCharArray ();
-         char[] result = new char[chars.Length];
-         int j = chars.Length - 1;
-         for (int i = 0; i < chars.Length; i++) {
-            if (chars[i] == ' ') result[i] = ' ';
-         }
-         for (int i = 0; i < chars.Length; i++) {
+         int len = chars.Length;
+         char[] result = new char[len];
+         int j = len - 1;
+         for (int i = 0; i < len; i++) if (chars[i] == ' ') result[i] = ' ';
+         for (int i = 0; i < len; i++) {
             if (chars[i] != ' ') {
                while (j >= 0 && result[j] == ' ') j--;
                result[j] = chars[i];
                j--;
             }
          }
-         for (int i = 0; i < chars.Length; i++) {
+         for (int i = 0; i < len; i++) {
             if (char.IsUpper (word[i])) result[i] = char.ToUpper (result[i]);
-            else if (char.IsLower (word[i])) result[i] = char.ToLower (result[i]);
+            else result[i] = char.ToLower (result[i]);
          }
          WriteLine (result);
       } else WriteLine ("No input provided.");

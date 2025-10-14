a97709f4d4cca2ce32e202c426ea358ad33bfc8e
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-14 12:56:15 +0530

    Review Fix 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -12,18 +12,25 @@ internal class Program {
    static void Main (string[] args) {
       Write ("Enter the number of rows upto which you wish to print the Pascal's triangle: ");
       int.TryParse (ReadLine (), out int r);
-      for (int i = 0; i < r; i++) {
-         Write (new string (' ', r - i - 1));
-         for (int j = 0; j <= i; j++) Write ($"{GetPosValue (i, j)} ");
-         WriteLine ();
+      if (r <= 5) {
+         for (int i = 0; i < r; i++) {
+            Write (new string (' ', r - i - 1));
+            for (int j = 0; j <= i; j++) Write ($"{GetPosValue (i, j)} ");
+            WriteLine ();
+         }
+      } else {
+         for (int i = 0; i < r; i++) {
+            for (int j = 0; j <= i; j++) Write ($"{GetPosValue (i, j)} ");
+            WriteLine ();
+         }
       }
-   }
 
-   // Each element in the pascal's triangle is calculated using the combination formula (nCr)
-   // where,r is the row number and n is the position of the element within the row
-   static int GetPosValue (int r, int n) {
-      int result = 1;
-      for (int i = 1; i <= n; i++) result = result * (r - i + 1) / i;
-      return result;
+      // Each element in the pascal's triangle is calculated using the combination formula (nCr)
+      // where,r is the row number and n is the position of the element within the row
+      static int GetPosValue (int r, int n) {
+         int result = 1;
+         for (int i = 1; i <= n; i++) result = result * (r - i + 1) / i;
+         return result;
+      }
    }
 }

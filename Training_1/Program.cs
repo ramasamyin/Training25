ce1116862de0ace9c1c3e6ae5e1fd3b0cc6fcc51
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-14 11:09:08 +0530

    Review Fix 3

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -9,15 +9,15 @@ using static System.Console;
 namespace Training_1;
 
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       Write ("Enter a string for which adjacent matching letters must be deleted: ");
       string? word = ReadLine ()?.ToLower ();
       if (!string.IsNullOrEmpty (word)) {
          int index = 0;
          var result = new char[word.Length];
          foreach (char c in word) {
-            if (index > 0 && result[index - 1] == c) { index--; } else { result[index] = c; index++; }
-
+            if (index > 0 && result[index - 1] == c) index--;
+            else result[index++] = c;
          }
          if (result.Length > 0) WriteLine ($"Output: {new string (result, 0, index)}");
          else WriteLine ("Empty string");

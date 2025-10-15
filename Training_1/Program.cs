d92a2fb261555aa34747b584267b109e9f9d836e
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-15 16:32:42 +0530

    Commit - 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -3,29 +3,31 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on T08 branch.
 // ------------------------------------------------------------------------------------------------
 using System.Text;
+using static System.Console;
+namespace Training_1;
 
-namespace Training_1 {
-   internal class Program {
-      static void Main (string[] args) {
-         StringBuilder sb = new ();
-         string? word = Console.ReadLine ();
-         char[] Special = new char[] { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+' };
-         if (string.IsNullOrEmpty (word)) Console.WriteLine ("You haven't entered anything.");
-         else {
-            if (word.Length < 6) Append (sb, "The length must be atleast 6.");
-            if (!word.Any (char.IsDigit)) Append (sb, "It must contain atleast one digit.");
-            if (!word.Any (char.IsUpper)) Append (sb, "It must contain atleast one Uppercase letter.");
-            if (!word.Any (char.IsLower)) Append (sb, "It must contain atleast one Lowercase letter.");
-            if (!Special.Any (s => word.Contains (s))) Append (sb, "It must contain atleast one special character (!@#$%^&*()-+).");
-            if (sb.Length > 0) Console.WriteLine ($"The password is not strong.\n{sb}");
-            else Console.WriteLine ("Password is strong");
-         }
+internal class Program {
+   static void Main () {
+      StringBuilder sb = new ();
+      Write ("Enter an input: ");
+      string? input = ReadLine ();
+      char[] spl = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'];
+      if (string.IsNullOrEmpty (input)) WriteLine ("Input is empty");
+      else {
+         if (input.Length < 6) Append (sb, "The length must be atleast 6.");
+         if (!input.Any (char.IsDigit)) Append (sb, "It must contain atleast one digit.");
+         if (!input.Any (char.IsUpper)) Append (sb, "It must contain atleast one Uppercase letter.");
+         if (!input.Any (char.IsLower)) Append (sb, "It must contain atleast one Lowercase letter.");
+         if (!spl.Any (s => input.Contains (s))) Append (sb, "It must contain atleast one special character (!@#$%^&*()-+).");
+         if (sb.Length > 0) WriteLine ($"The password is not strong.\n{sb}");
+         else WriteLine ("Password is strong");
       }
-
-      static void Append (StringBuilder sb, string str) => sb.AppendLine (str);
    }
+
+   // Adds the string to a string builder
+   static void Append (StringBuilder sb, string str) => sb.AppendLine (str);
 }
 

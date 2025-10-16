c64894e97f647305dee91db7a808130d23c445e5
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-16 11:54:38 +0530

    Review Fix 1

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -18,15 +18,53 @@ internal class Program {
       if (string.IsNullOrEmpty (input)) WriteLine ("Input is empty");
       else {
          if (input.Length < 6) Append (errors, "The length must be atleast 6.");
-         if (!input.Any (char.IsDigit)) Append (errors, "It must contain atleast one digit.");
-         if (!input.Any (char.IsUpper)) Append (errors, "It must contain atleast one Uppercase letter.");
-         if (!input.Any (char.IsLower)) Append (errors, "It must contain atleast one Lowercase letter.");
-         if (!splChars.Any (s => input.Contains (s))) Append (errors, "It must contain atleast one special character (!@#$%^&*()-+).");
+         if (!ContainsDigit (input)) Append (errors, "It must contain atleast one digit.");
+         if (!ContainsUppercase (input)) Append (errors, "It must contain atleast one Uppercase letter.");
+         if (!ContainsLowercase (input)) Append (errors, "It must contain atleast one Lowercase letter.");
+         if (!ContainsSpecialChar (input, splChars)) Append (errors, "It must contain atleast one special character (!@#$%^&*()-+).");
          WriteLine (errors.Length > 0 ? $"The password is not strong.\n{errors}" : "Password is strong");
       }
    }
 
    // Adds the string to a string builder
    static void Append (StringBuilder sb, string str) => sb.AppendLine (str);
+
+   //Returns true if there is a digit in the string
+   static bool ContainsDigit (string s) {
+      foreach (char c in s) {
+         if (char.IsDigit (c))
+            return true;
+      }
+      return false;
+   }
+
+   //Returns true if there is a lowercase alphabet in the string
+   static bool ContainsLowercase (string s) {
+      foreach (char c in s) {
+         if (char.IsLower (c))
+            return true;
+      }
+      return false;
+   }
+
+   //Returns true if there is a special character in the string
+   static bool ContainsSpecialChar (string s, char[] specialChars) {
+      foreach (char c in s) {
+         foreach (char sc in specialChars) {
+            if (c == sc)
+               return true;
+         }
+      }
+      return false;
+   }
+
+   //Returns true if there is a uppercase alphabet in the string
+   static bool ContainsUppercase (string s) {
+      foreach (char c in s) {
+         if (char.IsUpper (c))
+            return true;
+      }
+      return false;
+   }
 }
 

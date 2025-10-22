f7e107bdf5df27059ed565e1a5e77472f047ab7b
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-22 15:11:40 +0530

    Review Fix 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -9,6 +9,14 @@ using static System.Console;
 namespace Training_1;
 
 internal class Program {
+   private static readonly int[] RomanValues = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+   private static readonly string[] RomanSymbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+   private static readonly string[] Ones = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+   private static readonly string[] Teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+   private static readonly string[] Tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+   private static readonly string[] Places = ["crore", "lakh", "thousand", "hundred", ""];
+   private static readonly int[] Divisors = [10000000, 100000, 1000, 100, 1];
+
    static void Main () {
       Write ("Enter a number: ");
       int.TryParse (ReadLine (), out int input);
@@ -21,15 +29,12 @@ internal class Program {
 
    //Converts the given input number into roman numeral and prints it to the console
    static void ConvertToRoman (int input) {
-      int[] values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
-      string[] symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
       string roman = "";
-      int len = values.Length;
-      for (int i = 0; i < len; i++) {
-         int value = values[i];
+      for (int i = 0, len = RomanValues.Length; i < len; i++) {
+         int value = RomanValues[i];
          while (input >= value) {
             input -= value;
-            roman += symbols[i];
+            roman += RomanSymbols[i];
          }
       }
       WriteLine ($"Roman numeral: {roman}");
@@ -37,25 +42,20 @@ internal class Program {
 
    // Converts the given input number into words and prints it to the console
    static void ConvertToWord (int input) {
-      string[] ones = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
-      string[] teens = { "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-      string[] tens = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
-      string[] places = { "crore", "lakh", "thousand", "hundred", "" };
-      int[] divisors = { 10000000, 100000, 1000, 100, 1 };
       string word = "";
-      for (int i = 0; i < divisors.Length; i++) {
-         int value = input / divisors[i];
+      for (int i = 0, len = Divisors.Length; i < len; i++) {
+         int value = input / Divisors[i];
          if (value != 0) {
-            if (value < 10) word += ones[value] + " ";
-            else if (value < 20) word += teens[value - 10] + " ";
+            if (value < 10) word += Ones[value] + " ";
+            else if (value < 20) word += Teens[value - 10] + " ";
             else if (value < 100) {
-               word += tens[value / 10] + " ";
-               if (value % 10 != 0) word += ones[value % 10] + " ";
+               word += Tens[value / 10] + " ";
+               if (value % 10 != 0) word += Ones[value % 10] + " ";
             }
-            string place = places[i];
+            string place = Places[i];
             if (place != "") word += place + " ";
          }
-         input %= divisors[i];
+         input %= Divisors[i];
       }
       WriteLine ($"Number (in words): {word.Trim ().ToUpper ()}");
    }

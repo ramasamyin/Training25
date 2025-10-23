20a26c685abb3a00648ab94b9e0c25ef5e2c318c
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-23 15:23:58 +0530

    Commit - 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -7,20 +7,19 @@
 // ------------------------------------------------------------------------------------------------
 using static System.Console;
 namespace Training_1;
+
 internal class Program {
-   static void Main (string[] args) {
+   static void Main () {
       Write ("Enter a number: ");
       int.TryParse (ReadLine (), out int n);
       WriteLine (MinTransform (n));
    }
 
-   //Returns the smallest number of changes it takes to transform a number into a number with identical digits
-   //by incrementing or decrementing a digit by one.
+   // Function to calculate the minimum transformation cost by changing all digits to a single digit by calculating the
+   // absolute difference between each digit and the target digit.
    static int MinTransform (int num) {
       int minSum = int.MaxValue;
-      for (int k = 0; k < 10; k++) {
-         int temp = num;
-         int sum = 0;
+      for (int k = 0, temp = num, sum = 0; k < 10; k++) {
          while (temp > 0) {
             int digit = temp % 10;
             sum += Math.Abs (digit - k);

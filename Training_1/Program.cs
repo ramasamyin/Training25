078684f2eed54951a49c1e832a45304ad188cc24
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-29 11:20:23 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -3,12 +3,33 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on Test2 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training_1 {
-   internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+namespace Training_1;
+
+internal class Program {
+   static void Main (string[] args) {
+      int[] row1 = [.. Console.ReadLine ()!.Split (' ').Select (int.Parse)];
+      int[] row2 = [.. Console.ReadLine ()!.Split (' ').Select (int.Parse)];
+      int[] row3 = [.. Console.ReadLine ()!.Split (' ').Select (int.Parse)];
+      int[,] array2D = new int[3, 3] {
+                { row1[0], row1[1], row1[2] },
+                { row2[0], row2[1], row2[2] },
+                { row3[0], row3[1], row3[2] }
+         };
+      for (int i = 0; i < 3;) {
+         int sum1 = array2D[i, 0] + array2D[i, 1] + array2D[i, 2];
+         int sum2 = array2D[0, i] + array2D[1, i] + array2D[2, i];
+         int sum3 = array2D[0, 0] + array2D[1, 1] + array2D[2, 2];
+         int sum4 = array2D[0, 2] + array2D[1, 1] + array2D[2, 0];
+         if (sum1 == sum2 && sum2 == sum3 && sum3 == sum4) {
+            Console.WriteLine ("The given 3x3 matrix is a magic square.");
+            break;
+         } else {
+            Console.WriteLine ("The given 3x3 matrix is not a magic square.");
+            break;
+         }
       }
    }
 }
+

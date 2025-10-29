52ec735f254574a2aaa09898eb98efbaf6fd9702
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-29 11:32:34 +0530

    Commit 2

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -3,25 +3,24 @@
 // Copyright (c) Metamation India.
 // ------------------------------------------------------------------
 // Program.cs
-// Program on main branch.
+// Program on Test3 branch.
 // ------------------------------------------------------------------------------------------------
-namespace Training_1 {
-   internal class Program {
-      static void Main () {
-         int n;
-         Console.Write ("Enter a number: ");
-         while (!int.TryParse (Console.ReadLine (), out n)) Console.WriteLine ("Please enter a valid input");
-         List<int> even = [];
-         List<int> odd = [];
-         int digit;
-         while (n > 0) {
-            digit = n % 10;
-            if (digit % 2 == 0) even.Add (digit);
-            else odd.Add (digit);
-            n /= 10;
-         }
-         Console.Write (String.Join ("", even.OrderBy (x => x)));
-         Console.Write (String.Join ("", odd.OrderBy (x => x)));
+namespace Training_1;
+
+internal class Program {
+   static void Main () {
+      int n;
+      Console.Write ("Enter a number: ");
+      while (!int.TryParse (Console.ReadLine (), out n)) Console.WriteLine ("Please enter a valid input");
+      List<int> even = [];
+      List<int> odd = [];
+      while (n > 0) {
+         int digit = n % 10;
+         if (digit % 2 == 0) even.Add (digit);
+         else odd.Add (digit);
+         n /= 10;
       }
+      Console.Write ($"{String.Join ("", even.OrderBy (x => x))}{String.Join ("", odd.OrderBy (x => x))}");
    }
 }
+

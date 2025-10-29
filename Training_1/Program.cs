71438ebcfd346ccb33fdbffcdb235b40da11b3de
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-29 10:46:30 +0530

    Initial commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -7,8 +7,21 @@
 // ------------------------------------------------------------------------------------------------
 namespace Training_1 {
    internal class Program {
-      static void Main (string[] args) {
-         Console.WriteLine ("Hello, World!");
+      static void Main () {
+         int n;
+         Console.Write ("Enter a number: ");
+         while (!int.TryParse (Console.ReadLine (), out n)) Console.WriteLine ("Please enter a valid input");
+         List<int> even = [];
+         List<int> odd = [];
+         int digit;
+         while (n > 0) {
+            digit = n % 10;
+            if (digit % 2 == 0) even.Add (digit);
+            else odd.Add (digit);
+            n /= 10;
+         }
+         Console.Write (String.Join ("", even.OrderBy (x => x)));
+         Console.Write (String.Join ("", odd.OrderBy (x => x)));
       }
    }
 }

cc303fa0f3fddcd2109d6f65dfab491baf36b1d3
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-10-29 12:55:17 +0530

    Final commit

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -9,14 +9,17 @@ namespace Training_1;
 
 internal class Program {
    static void Main () {
+      Console.WriteLine ("Enter row-1 elements separated by a space");
       int[] row1 = [.. Console.ReadLine ()!.Split (' ').Select (int.Parse)];
+      Console.WriteLine ("Enter row-2 elements separated by a space");
       int[] row2 = [.. Console.ReadLine ()!.Split (' ').Select (int.Parse)];
+      Console.WriteLine ("Enter row-3 elements separated by a space");
       int[] row3 = [.. Console.ReadLine ()!.Split (' ').Select (int.Parse)];
       int[,] array2D = new int[3, 3] {
                 { row1[0], row1[1], row1[2] },
                 { row2[0], row2[1], row2[2] },
                 { row3[0], row3[1], row3[2] }
-         };
+      };
       for (int i = 0; i < 3;) {
          int sum1 = array2D[i, 0] + array2D[i, 1] + array2D[i, 2];
          int sum2 = array2D[0, i] + array2D[1, i] + array2D[2, i];

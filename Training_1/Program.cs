ed721bf3f2c95ed491a44e36126e05643c9d32d0
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-11-03 16:12:36 +0530

    Review Fix 7

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -12,15 +12,15 @@ internal class Program {
    static void Main () {
       Write ("Enter a number: ");
       if (int.TryParse (ReadLine (), out int n)) {
-         var (minSum, convertedNumber) = MinTransform (n);
+         var (minSum, bestTarget) = MinTransform (n);
          WriteLine ($"Minimum cost: {minSum}");
-         WriteLine ($"Transformed number: {convertedNumber}");
+         WriteLine ($"Transformed digit : {bestTarget}");
       } else WriteLine ("Invalid input");
    }
 
    // Function to calculate the minimum transformation cost by changing all digits to a single digit by calculating the
    // absolute difference between each digit and the target digit
-   static (int MinSum, string ConvertedNumber) MinTransform (int n) {
+   static (int MinSum, int bestTarget) MinTransform (int n) {
       HashSet<int> uniqueDigits = [];
       List<int> digits = new (10);
       int temp = n;
@@ -30,12 +30,12 @@ internal class Program {
          digits.Add (rem);
          temp /= 10;
       }
-      int minSum = int.MaxValue, bestTarget = 0, nLength = (int)Math.Floor (Math.Log10 (n)) + 1;
+      int minSum = int.MaxValue, bestTarget = 0;
       foreach (int target in uniqueDigits) {
          int sum = 0;
          foreach (int digit in digits) sum += Math.Abs (digit - target);
          if (sum < minSum) (minSum, bestTarget) = (sum, target);
       }
-      return (minSum, new string (bestTarget.ToString ()[0], nLength));
+      return (minSum, bestTarget);
    }
 }

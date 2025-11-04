52aefe88556f6787c2e8bdecf972c273dbcf7975
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-11-04 15:16:54 +0530

    Review Fix 3

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -11,24 +11,27 @@ namespace Training_1;
 
 internal class Program {
    static void Main () {
-      Console.OutputEncoding = new UnicodeEncoding ();
+      OutputEncoding = new UnicodeEncoding ();
+      const int BoardSize = 8;
       char[] whitePieces = { '\u2656', '\u2658', '\u2657', '\u2655', '\u2654', '\u2657', '\u2658', '\u2656' };
       char[] blackPieces = { '\u265C', '\u265E', '\u265D', '\u265B', '\u265A', '\u265D', '\u265E', '\u265C' };
       char whitePawn = '\u2659', blackPawn = '\u265F';
-      char[,] board = new char[8, 8];
-      for (int i = 0; i < 8; i++) {
-         for (int j = 0; j < 8; j++) {
-            if (i == 0) board[i, j] = blackPieces[j];
-            else if (i == 1) board[i, j] = blackPawn;
-            else if (i == 6) board[i, j] = whitePawn;
-            else if (i == 7) board[i, j] = whitePieces[j];
-            else board[i, j] = ' ';
+      char[,] board = new char[BoardSize, BoardSize];
+      for (int i = 0; i < BoardSize; i++) {
+         for (int j = 0; j < BoardSize; j++) {
+            board[i, j] = i switch {
+               0 => blackPieces[j],
+               1 => blackPawn,
+               6 => whitePawn,
+               7 => whitePieces[j],
+               _ => ' '
+            };
          }
       }
       WriteLine ("┌────┬────┬────┬────┬────┬────┬────┬────┐");
-      for (int i = 0; i < 8; i++) {
+      for (int i = 0; i < BoardSize; i++) {
          Write ("│");
-         for (int j = 0; j < 8; j++) Write ($" {board[i, j]}  │");
+         for (int j = 0; j < BoardSize; j++) Write ($" {board[i, j]}  │");
          WriteLine ();
          if (i < 7) WriteLine ("├────┼────┼────┼────┼────┼────┼────┼────┤");
       }

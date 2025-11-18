53842726443bdb952d01c4bf614090adffbfddd7
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-11-18 14:24:55 +0530

    Review Fix: Dict

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -60,15 +60,13 @@ internal class Program {
    #endregion
 
    #region Fields ---------------------------------------------------
-   static Dictionary<int, string> sPlaceByDivisor = new () {
-      [(int)1E5] = "lakh", [(int)1E3] = "thousand", [100] = "hundred", [1] = ""
-   };
    static int[] sdivisors = [100000, 1000, 100, 1];
    static string[] sOnes = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
    static string[] sTeens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
    static string[] sTens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
-   static Dictionary<int, string> sRomanMap = new () { { 1000,"M"},{ 900,"CM"},{ 500,"D"},{ 400,"CD"},{ 100,"C"},{ 90,"XC"},
-                                                      { 50,"L"},{ 40,"XL"},{ 10,"X"},{ 9,"IX"},{ 5,"V"},{ 4,"IV"},{ 1,"I"} };
+   static Dictionary<int, string> sPlaceByDivisor = new () { [(int)1E5] = "lakh", [(int)1E3] = "thousand", [100] = "hundred", [1] = "" },
+                                  sRomanMap = new () { { 1000,"M"},{ 900,"CM"},{ 500,"D"},{ 400,"CD"},{ 100,"C"},{ 90,"XC"},
+                                                       { 50,"L"},{ 40,"XL"},{ 10,"X"},{ 9,"IX"},{ 5,"V"},{ 4,"IV"},{ 1,"I"} };
    #endregion
 }
 #endregion

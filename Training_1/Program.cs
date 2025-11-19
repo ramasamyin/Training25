566d19093ff33d781e24284eeecb86fa468c381f
Indumeenal <Indumeenal.Ramasamy@trumpf.com>
2025-11-19 12:56:22 +0530

    Added regions

--- Training_1/Program.cs
+++ Training_1/Program.cs
@@ -8,6 +8,7 @@
 using static System.Console;
 namespace Training_1;
 
+#region class Program -----------------------------------------------------------------------------
 internal class Program {
    static void Main () {
       List<char> oldList = [];
@@ -39,76 +40,83 @@ internal class Program {
       Write ("Custom list elements: ");
       for (int i = 0; i < newList.Count; i++) Write ($"{newList[i]} ");
    }
+}
+#endregion
 
-   class MyList<T> {
-      private T[] _items;
-      private int _count;
+#region class MyList<T> ---------------------------------------------------------------------------
+class MyList<T> {
+   private T[] _items;
+   private int _count;
 
-      public MyList () {
-         _items = new T[4];
-         _count = 0;
+   public MyList () {
+      _items = new T[4];
+      _count = 0;
+   }
+
+   #region Properties -----------------------------------------------
+   public int Count => _count;
+   public int Capacity => _items.Length;
+
+   // Indexer
+   public T this[int index] {
+      get {
+         if (index < 0 || index >= _count) throw new IndexOutOfRangeException ("Index out of range.");
+         return _items[index];
       }
-
-      public int Count => _count;
-      public int Capacity => _items.Length;
-
-      // Indexer
-      public T this[int index] {
-         get {
-            if (index < 0 || index >= _count) throw new IndexOutOfRangeException ("Index out of range.");
-            return _items[index];
-         }
-         set {
-            if (index < 0 || index >= _count) throw new IndexOutOfRangeException ("Index out of range.");
-            _items[index] = value;
-         }
-      }
-
-      // Add element to the end
-      public void Add (T a) {
-         if (_count == _items.Length)
-            Resize (); // double capacity
-         _items[_count] = a;
-         _count++;
-      }
-
-      // Remove first occurrence of the element
-      public bool Remove (T a) {
-         int index = Array.IndexOf (_items, a, 0, _count);
-         if (index == -1)
-            return false;
-         RemoveAt (index);
-         return true;
-      }
-
-      // Clear all elements
-      public void Clear () {
-         _items = new T[4];
-         _count = 0;
-      }
-
-      // Insert element at a given index
-      public void Insert (int index, T a) {
-         if (index < 0 || index > _count) throw new ArgumentOutOfRangeException (nameof (index), "Invalid index for insert.");
-         if (_count == _items.Length) Resize ();
-         for (int i = _count; i > index; i--) _items[i] = _items[i - 1];
-         _items[index] = a;
-         _count++;
-      }
-
-      // Remove element at a given index
-      public void RemoveAt (int index) {
-         if (index < 0 || index >= _count) throw new ArgumentOutOfRangeException (nameof (index), "Invalid index for removal.");
-         for (int i = index; i < _count - 1; i++) _items[i] = _items[i + 1];
-         _items[_count - 1] = default!; _count--;
-      }
-
-      // Double the capacity
-      private void Resize () {
-         int newCapacity = _items.Length * 2;
-         T[] newArray = new T[newCapacity];
-         Array.Copy (_items, newArray, _count);
-         _items = newArray;
+      set {
+         if (index < 0 || index >= _count) throw new IndexOutOfRangeException ("Index out of range.");
+         _items[index] = value;
       }
    }
+   #endregion
+
+   #region Implementation -------------------------------------------
+   // Add element to the end
+   public void Add (T a) {
+      if (_count == _items.Length)
+         Resize (); // double capacity
+      _items[_count] = a;
+      _count++;
+   }
+
+   // Remove first occurrence of the element
+   public bool Remove (T a) {
+      int index = Array.IndexOf (_items, a, 0, _count);
+      if (index == -1)
+         return false;
+      RemoveAt (index);
+      return true;
+   }
+
+   // Clear all elements
+   public void Clear () {
+      _items = new T[4];
+      _count = 0;
+   }
+
+   // Insert element at a given index
+   public void Insert (int index, T a) {
+      if (index < 0 || index > _count) throw new ArgumentOutOfRangeException (nameof (index), "Invalid index for insert.");
+      if (_count == _items.Length) Resize ();
+      for (int i = _count; i > index; i--) _items[i] = _items[i - 1];
+      _items[index] = a;
+      _count++;
+   }
+
+   // Remove element at a given index
+   public void RemoveAt (int index) {
+      if (index < 0 || index >= _count) throw new ArgumentOutOfRangeException (nameof (index), "Invalid index for removal.");
+      for (int i = index; i < _count - 1; i++) _items[i] = _items[i + 1];
+      _items[_count - 1] = default!; _count--;
+   }
+
+   // Double the capacity
+   private void Resize () {
+      int newCapacity = _items.Length * 2;
+      T[] newArray = new T[newCapacity];
+      Array.Copy (_items, newArray, _count);
+      _items = newArray;
+   }
+   #endregion
 }
+#endregion
